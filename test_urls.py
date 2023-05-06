afb989fa711001556f3e23c455cffe19bb31af5a
Ceyda Duzgec <cduzgec@gmail.com>
2023-05-06 15:20:05 +0300

    Start on parsing parameters

--- test_urls.py
+++ test_urls.py
@@ -1,35 +1,66 @@
+from cgi import test
 import json
 import requests
 from swagger_parser import SwaggerParser
 
 
-def get_urls_from_txt():
+def get_json_urls_from_txt():
     with open("test_sites.txt", "r") as file:
-        urls = [line.strip() for line in file.readlines()]
-    return urls
+        json_urls = [line.strip() for line in file.readlines()]
+    return json_urls
 
 
 def get_swagger_data_for_each_url():
     url_results = {}
-    urls = get_urls_from_txt()
-    for url in urls:
-        response = requests.get(url)
+    json_urls = get_json_urls_from_txt()
+    for json_url in json_urls:
+        response = requests.get(json_url)
         if response.status_code == 200:
-            url_results[url] = response.json()
+            res_json = response.json()
+            base_url = res_json["schemes"][0] + "://" + res_json["host"]
+            # The second scheme is ignored, can be implemented later
+            url_results[base_url] = res_json
+        else:
+            assert False, f"Failed to get swagger data for {json_url}"
     return url_results
-
-
-def parse_json_content(data):
-    return data
 
 
 def test_urls():
     url_results = get_swagger_data_for_each_url()
     for url_name in url_results:
-        print(f"Testing: {url_name}")
+        print(f"Parsing: {url_name}")
         parser = SwaggerParser(swagger_dict=url_results[url_name])
-        spec = parser.specification
-        print(spec['info']['version'])
+
+        for path in parser.paths:
+            api = url_name + path
+            print(f"Testing: {api}")
+            for method in parser.paths[path]:
+                print(f"Testing: {api} - {method}")
+                parameters = parser.paths[path][method]["parameters"]
+                parameter_values = {}
+                for par in parameters:
+                    if parameters[par]["required"] and parameters[par]["in"] == "path":
+                        if parameters[par]["type"] == "string":
+                            parameter_values[parameters[par]["name"]] = "test"
+                        elif parameters[par]["type"] == "integer":
+                            parameter_values[parameters[par]["name"]] = 123
+                        elif parameters[par]["type"] == "boolean":
+                            parameter_values[parameters[par]["name"]] = True
+
+                response = requests.request(method=method, url=api)
+
+                if response.status_code == 200:
+                    print(f"PASSED - {api} - {method}")
+                else:
+                    print(f"FAILED - {api} - {method}")
+
+
+
+
+
+
+
+
 
 
 test_urls()
