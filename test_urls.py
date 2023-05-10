a1cf99965e8eed96c6565f3072019f2b1b658396
Ceyda Duzgec <cduzgec@gmail.com>
2023-05-10 14:31:59 +0300

    Implement body parameter

--- test_urls.py
+++ test_urls.py
@@ -27,32 +27,44 @@ def get_swagger_data_for_each_url():
 
 def test_urls():
     url_results = get_swagger_data_for_each_url()
-    for url_name in url_results:
-        print(f"Parsing: {url_name}")
-        parser = SwaggerParser(swagger_dict=url_results[url_name])
+    for base_url in url_results:
+        print(f"-------- Parsing: {base_url} -------")
+        parser = SwaggerParser(swagger_dict=url_results[base_url])
 
         for path in parser.paths:
-            api = url_name + path
-            print(f"Testing: {api}")
+            api = base_url + path
             for method in parser.paths[path]:
-                print(f"Testing: {api} - {method}")
-                parameters = parser.paths[path][method]["parameters"]
                 parameter_values = {}
+                data = None
+                headers = None
+                #print(f"Testing: {api} - {method}")
+                parameters = parser.paths[path][method]["parameters"]
+
                 for par in parameters:
-                    if parameters[par]["required"] and parameters[par]["in"] == "path":
+                    if parameters[par]["in"] == "path" and parameters[par]["required"]:
                         if parameters[par]["type"] == "string":
                             parameter_values[parameters[par]["name"]] = "test"
                         elif parameters[par]["type"] == "integer":
-                            parameter_values[parameters[par]["name"]] = 123
+                            parameter_values[parameters[par]["name"]] = "9222968140491042141"
                         elif parameters[par]["type"] == "boolean":
-                            parameter_values[parameters[par]["name"]] = True
+                            parameter_values[parameters[par]["name"]] = "True"
 
-                response = requests.request(method=method, url=api)
+                        for api_parameter in parameter_values:
+                            if api.find("{") != -1:
+                                api = api.split("{")[0] + parameter_values[api_parameter] + api.split("}")[1]
+
+
+                    elif parameters[par]["in"] == "body" and parameters[par]["required"]:
+                        model = parameters[par]["schema"]["$ref"].split("/")[2]
+                        data = json.dumps(parser.definitions_example[model])
+                        headers = {"Content-Type": parser.paths[path][method]["consumes"][0]}
+
+                response = requests.request(method=method, url=api, data=data, headers=headers)
 
                 if response.status_code == 200:
-                    print(f"PASSED - {api} - {method}")
+                    print(f"PASSED: {api} - {method}")
                 else:
-                    print(f"FAILED - {api} - {method}")
+                    print(f"FAILED: {api} - {method}")
 
 
 
